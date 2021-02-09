adda30f1c73babf10896109396c26d6be6cb08eb
Jafar.Mahmoudi <Jafar.Mahmoudi@playtech.com>
2021-02-09 15:29:56 +0100

    updated product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, HttpStatus } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiOperation({ description: 'Create new product' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The product has been successfully created.' /*, type: T*/,
+  })
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
